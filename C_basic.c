1bbd0a89b9198ed8a7d9a7f0621fdcf824ffce0e
Shihab Hossain <hossainshihab2001@gmail.com>
2024-12-31 12:03:53 +0600

    Add files via upload

---
+++ C_basic.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 5; // initialization
+    int a;     // declaration
+    a = 10;    // assignment
+
+    int x, y;
+
+    int ch;
+    scanf("%d %d %c", &y, &x, &ch);
+    printf("Output x: %d\nOutput y: %d\n Character: %c", y, x, ch);
+    // printf("Output y: %d", y);
+
+    return 0;
+}
+
+/*
+int = %d
+float = %f
+double = %lf
+char = %c
+long long int = %lld
+long int = %ld
+ */
+
+
+// preprossesing unit
+// #include <stdbool.h>
+
+// int main()
+// {
+//     int n = 5;
+//     float f = 5.67;
+//     int ft = 5.67;
+//     char ch = '1';
+//     bool opt = true;
+
+//     return 0;
+// }
---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+   int a,b;
+   scanf("%d", &a);
+   scanf("%d", &b);
+    printf("shihab= %d %d", a , b);
+
+    return 0;
+}
